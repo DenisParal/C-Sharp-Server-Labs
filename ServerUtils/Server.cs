b829eb83ff017fa67f8b21ceb640cee7cccff87f
dparanic <den.par.al@yandex.com>
2021-05-21 00:54:50 +0300

    Changed Namespace for server utils

--- ServerUtils/Server.cs
+++ ServerUtils/Server.cs
@@ -7,7 +7,7 @@ using System.Threading;
 using System.Net;
 using System.Net.Sockets;
 
-namespace SocketTCPServer
+namespace ServerUtils
 {
     abstract class Server
     {
@@ -84,35 +84,5 @@ namespace SocketTCPServer
         protected Socket socket;
         protected bool serverCondition = false;
         protected Thread thread;
-    }
-
-    class ServerEcho : Server
-    {
-        public ServerEcho(String IP = "127.0.0.1", int Port = 8080) : base(IP, Port) { }
-        public ServerEcho(IPAddress IP, int Port = 8080) : base(IP, Port) { }
-        protected override void ServerWork()
-        {
-            socket.Listen(1);
-            if(socket.Connected)
-            {
-                Socket handler = socket.Accept();
-                StringBuilder builder = new StringBuilder();
-                int bytes = 0;
-                byte[] data = new byte[256];
-                do
-                {
-                    bytes = handler.Receive(data);
-                    builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
-                }
-                while (handler.Available > 0);
-                Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + builder.ToString());
-
-                string message = builder.ToString();
-                data = Encoding.Unicode.GetBytes(message);
-                handler.Send(data);
-                handler.Shutdown(SocketShutdown.Both);
-                handler.Close();
-            }
-        }
     }
 }
